33513fc58cad9522c3c755e92d20769f2f542789
louis gueye <louis.gueye@gmail.com>
2020-02-04 00:13:21 +0100

    wip: broker-server

--- blotter-broker/blotter-broker-server/src/main/java/org/organization/blotter/broker/server/BlotterBrokerServerConfiguration.java
+++ blotter-broker/blotter-broker-server/src/main/java/org/organization/blotter/broker/server/BlotterBrokerServerConfiguration.java
@@ -1,5 +1,8 @@
 package org.organization.blotter.broker.server;
 
+import org.apache.activemq.broker.BrokerService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -7,4 +10,15 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class BlotterBrokerServerConfiguration {
+
+	@Bean
+	public BrokerService brokerService(@Value("${spring.activemq.broker-url}") final String brokerUrl) throws Exception {
+		BrokerService broker = new BrokerService();
+		broker.addConnector(brokerUrl);
+		broker.setPersistent(false);
+		broker.setUseJmx(false);
+		broker.start();
+		return broker;
+	}
+
 }
