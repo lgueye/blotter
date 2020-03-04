3d171823536fbf7ba440cd2d6a077c79b6342ed9
louis gueye <louis.gueye@gmail.com>
2020-03-04 06:58:18 +0100

    wip #8 (cover with tests)

--- blotter-api/blotter-api-server/src/test/java/org/organization/blotter/api/server/normalized/SearchOrdersResourceTest.java
+++ blotter-api/blotter-api-server/src/test/java/org/organization/blotter/api/server/normalized/SearchOrdersResourceTest.java
@@ -12,7 +12,6 @@ import org.organization.blotter.api.model.SearchOrderCriteria;
 import org.organization.blotter.shared.configuration.BlotterSharedSerializationConfiguration;
 import org.organization.blotter.shared.model.OrderStatus;
 import org.organization.blotter.store.client.BlotterStoreClient;
-import org.springframework.data.domain.Example;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -23,11 +22,8 @@ import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.Instant;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -36,47 +32,46 @@ import static org.mockito.Mockito.when;
  */
 @RunWith(MockitoJUnitRunner.class)
 public class SearchOrdersResourceTest {
-    private final String API_URL = "http://foo.bar";
-    @Mock
-    private BlotterStoreClient service;
-    private ObjectMapper objectMapper;
-    private MockMvc mockMvc;
+	private final String API_URL = "http://foo.bar";
+	@Mock
+	private BlotterStoreClient service;
+	private ObjectMapper objectMapper;
+	private MockMvc mockMvc;
 
-    @Before
-    public void before() {
-        SearchOrdersResource underTest = new SearchOrdersResource(service);
-        objectMapper = new BlotterSharedSerializationConfiguration().objectMapper();
-        mockMvc = MockMvcBuilders.standaloneSetup(underTest).setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper)).build();
-    }
+	@Before
+	public void before() {
+		SearchOrdersResource underTest = new SearchOrdersResource(service);
+		objectMapper = new BlotterSharedSerializationConfiguration().objectMapper();
+		mockMvc = MockMvcBuilders.standaloneSetup(underTest).setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper)).build();
+	}
 
+	@Test
+	public void findByCriteria() throws Exception {
+		// Given
+		final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+		final String portfolios = "pf-001,pf-002";
+		final String instruments = "inst-0005,inst-0007";
+		final String statuses = "placed,validated";
+		final String authors = "louis,alban";
+		params.add("portfolios", portfolios);
+		params.add("instruments", instruments);
+		params.add("statuses", statuses);
+		params.add("authors", authors);
+		final MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(UriComponentsBuilder.fromHttpUrl(API_URL)
+				.path("/api/v1/orders").queryParams(params).build().toUri());
+		final List<OrderReadDto> expected = Lists.newArrayList(
+				OrderReadDto.builder().portfolio("pf-001").instrument("inst-0005").status(OrderStatus.validated).author("louis").build(),
+				OrderReadDto.builder().portfolio("pf-002").instrument("inst-0007").status(OrderStatus.placed).author("alban").build());
+		final SearchOrderCriteria criteria = SearchOrderCriteria.builder().portfolios(portfolios).instruments(instruments).statuses(statuses)
+				.authors(authors).build();
+		when(service.findByCriteria(criteria)).thenReturn(expected);
 
-    @Test
-    public void findByCriteria() throws Exception {
-        // Given
-        final MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
-        final String portfolios = "pf-001,pf-002";
-        final String instruments = "inst-0005,inst-0007";
-        final String statuses = "placed,validated";
-        final String authors = "louis,alban";
-        params.add("portfolios", portfolios);
-        params.add("instruments", instruments);
-        params.add("statuses", statuses);
-        params.add("authors", authors);
-        final MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get(UriComponentsBuilder.fromHttpUrl(API_URL)
-                .path("/api/v1/orders").queryParams(params).build().toUri());
-        final List<OrderReadDto> expected = Lists.newArrayList(
-                OrderReadDto.builder().portfolio("pf-001").instrument("inst-0005").status(OrderStatus.validated).author("louis").build(),
-                OrderReadDto.builder().portfolio("pf-002").instrument("inst-0007").status(OrderStatus.placed).author("alban").build()
-        );
-        final SearchOrderCriteria criteria = SearchOrderCriteria.builder().portfolios(portfolios).instruments(instruments).statuses(statuses).authors(authors).build();
-        when(service.findByCriteria(criteria)).thenReturn(expected);
+		// When
+		mockMvc.perform(requestBuilder) //
+				.andExpect(MockMvcResultMatchers.status().isOk()) //
+				.andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expected)));
 
-        // When
-        mockMvc.perform(requestBuilder) //
-                .andExpect(MockMvcResultMatchers.status().isOk()) //
-                .andExpect(MockMvcResultMatchers.content().json(objectMapper.writeValueAsString(expected)));
-
-        verify(service).findByCriteria(criteria);
-    }
+		verify(service).findByCriteria(criteria);
+	}
 
 }
